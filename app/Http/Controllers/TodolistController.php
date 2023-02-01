7fdc39b5a32eef929e2f341a023c293568f1cd2c
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-02-01 15:17:56 +0800

    create todolist controller

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    public function todolist(Request $request)
+    {
+        # code...
+    }
+
+    public function addTodo(Request $request)
+    {
+        # code...
+    }
+
+    public function removeTodo(Request $request, string $id)
+    {
+        # code...
+    }
+}
