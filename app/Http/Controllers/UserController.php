460dabefedab77ffeb9ed9d289958b864156bf27
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-02-01 09:29:43 +0800

    create login page

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    public function login(): Response
+    {
+       return response()
+        ->view('user.login', [
+            "title" => "Login"
+        ]);
+    }
+
+    public function doLogin()
+    {
+        # code...
+    }
+
+    public function doLogout()
+    {
+        # code...
+    }
+}
